52c04e381391783bcda0bf3fe7677a742896f402
Samuel <samuelyongw@gmail.com>
2022-11-19 23:53:53 +1100

    Added post page and js, working, modified homepage post card layout slightly

--- public/js/logout.js
+++ public/js/logout.js
@@ -15,3 +15,6 @@ const logout = async () => {
 document
   .querySelector('#logout')
   .addEventListener('click', logout);
+  // Doing logout() may automatically call the function upon the page rendering
+  // Had this issue when signing in, it would document.location.replace('/'); then immediately logout().
+  // Function does not work when #logout is an "anchor" tag. Therefore, changed to "button"
