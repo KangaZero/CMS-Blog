4c0c9932a10199c48b7cdd9a67458c379d392665
Samuel <samuelyongw@gmail.com>
2022-11-21 12:26:37 +1100

    Another heroku fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const blogPostRoutes = require('./blogPostRoutes.js');
+const blogPostRoutes = require('./blogPostRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/posts', blogPostRoutes);
+router.use('/posts', blogPostRoutes);
 // router.use('/comments', commentRoutes);
 
 module.exports = router;
