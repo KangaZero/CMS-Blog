ab93a545bee014ceffbca730418cd0a2a14f37d9
Samuel <samuelyongw@gmail.com>
2022-11-21 12:55:38 +1100

    github naming fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
+// was './BlogPostRoutes.js' on github
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
