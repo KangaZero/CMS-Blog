49320c3e5b3164b11a6bf1f258fdf3131bdfb59d
Samuel <samuelyongw@gmail.com>
2022-11-21 12:10:19 +1100

    Revert changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-import blogPostRoutes from "./blogPostRoutes";
-// const blogPostRoutes = require('./blogPostRoutes');
+const blogPostRoutes = require('./blogPostRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
