1440705c3f79e9f575139618d895644f6d43954f
Samuel <samuelyongw@gmail.com>
2022-11-21 12:07:06 +1100

    small changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogPostRoutes = require('./blogPostRoutes');
+import blogPostRoutes from "./blogPostRoutes";
+// const blogPostRoutes = require('./blogPostRoutes');
 // const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
